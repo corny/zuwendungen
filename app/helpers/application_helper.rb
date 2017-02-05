d7cfb3bda3934cec114a476d983ebc3985b5fb9e
Julian Kornberger <jk+github@digineo.de>
2017-02-05 20:14:29 +0100

    Add styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def title_content(&block)
+    content = capture(&block)
+    content_for :title, content
+    content
+  end
+
+  def uniq_states
+    @uniq_states ||= Donation.states
+  end
+
 end
