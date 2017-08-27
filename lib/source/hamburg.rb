945f1994b98c2e389eb6e0fa357b829a90757bb3
Julian Kornberger <jk+github@digineo.de>
2017-08-27 22:15:25 +0200

    Hamburg: Add source for 2017-Q2

--- lib/source/hamburg.rb
+++ lib/source/hamburg.rb
@@ -17,6 +17,7 @@ module Source
         "2016-Q3.xlsx" => "http://daten.transparenz.hamburg.de/Dataport.HmbTG.ZS.Webservice.GetRessource100/GetRessource100.svc/6d17cea7-f22b-4a57-a58b-7b681bca9cea/Zuwendungsvorgaenge_2016_Quartal_3.xlsx",
         "2016-Q4.xlsx" => "http://daten.transparenz.hamburg.de/Dataport.HmbTG.ZS.Webservice.GetRessource100/GetRessource100.svc/4fea0df1-90fc-47e0-b765-db5314c93835/Zuwendungsvorgaenge_2016_Quartal_4.xlsx",
         "2017-Q1.xlsx" => "http://daten.transparenz.hamburg.de/Dataport.HmbTG.ZS.Webservice.GetRessource100/GetRessource100.svc/c6659048-d0e9-4edc-8d0a-161868d20151/Zuwendungsvorgaenge_2017_Quartal_1.xlsx",
+        "2017-Q2.xlsx" => "http://daten.transparenz.hamburg.de/Dataport.HmbTG.ZS.Webservice.GetRessource100/GetRessource100.svc/544cccf0-e804-4702-9139-a7391c4cd08c/Zuwendungsvorgaenge_2017_Quartal_2.xlsx",
       }
     end
 
