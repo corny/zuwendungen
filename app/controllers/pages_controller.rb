7f72073cb27cde8b6c743d1939b344cada070e5d
Julian Kornberger <jk+github@digineo.de>
2017-08-27 21:16:34 +0200

    Move SQL query to controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,7 @@
 class PagesController < ApplicationController
+
+  def state
+    @donations = Donation.where(state: params[:id]).grouped_by_recipients.joins(:recipient).order("amount DESC").limit(50)
+  end
+
 end
